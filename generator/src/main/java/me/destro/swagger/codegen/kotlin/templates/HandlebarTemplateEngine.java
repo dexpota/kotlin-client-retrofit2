20b9bfd1008d5907b3b9bd776a7896f0321dd6e2
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-06-09 12:32:26 +0200

    Updated templates and added helper funtion.

--- generator/src/main/java/me/destro/swagger/codegen/kotlin/templates/HandlebarTemplateEngine.java
+++ generator/src/main/java/me/destro/swagger/codegen/kotlin/templates/HandlebarTemplateEngine.java
@@ -1,12 +1,15 @@
 package me.destro.swagger.codegen.kotlin.templates;
 
 import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
 import com.github.jknack.handlebars.helper.StringHelpers;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.FileTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 import io.swagger.codegen.v3.CodegenConfig;
 import io.swagger.codegen.v3.CodegenConstants;
+import io.swagger.codegen.v3.CodegenParameter;
 import io.swagger.codegen.v3.templates.TemplateEngine;
 import org.apache.commons.lang3.StringUtils;
 
@@ -46,6 +49,24 @@ public class HandlebarTemplateEngine implements TemplateEngine {
         }
         final Handlebars handlebars = new Handlebars(templateLoader);
         handlebars.registerHelper("upper", StringHelpers.upper);
+
+        handlebars.registerHelper("annotation", new Helper<CodegenParameter>() {
+            @Override
+            public CharSequence apply(CodegenParameter context, Options options) throws IOException {
+                if (context.getIsBodyParam()) {
+                    return "@Body";
+                }else if (context.getIsHeaderParam()) {
+                    return new Handlebars.SafeString(String.format("@Header(\"%s\")", context.baseName));
+                }else if (context.getIsPathParam()) {
+                    return new Handlebars.SafeString(String.format("@Path(\"%s\")", context.baseName));
+                }else if (context.getIsQueryParam()) {
+                    return new Handlebars.SafeString(String.format("@Query(\"\")", context.baseName));
+                }else {
+                    return "";
+                }
+            }
+        });
+
         handlebars.prettyPrint(true);
         config.addHandlebarHelpers(handlebars);
         return handlebars.compile(templateFile);
